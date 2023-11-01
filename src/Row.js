ee9564e9b33af0bb02a918d02cbfa80103771383
Shafeeqsha1510 <shafeeqsha1510@gmail.com>
2023-11-01 14:50:30 +0530

    Row component structure created

---
+++ src/Row.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import "./Row.css";
+function Row() {
+  return (
+    <div className="row">
+      <h2>Title</h2>
+      <div className="row_posters">
+       
+           <img src='' />
+       
+      </div>
+      <div style={{ padding: "40px" }}>
+         
+      </div>
+    </div>
+  );
+}
+
+export default Row;
