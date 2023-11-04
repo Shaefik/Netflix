0506f36c4d124d4dda2ebe5dcf7c80d9fdc2728b
Shafeeqsha1510 <shafeeqsha1510@gmail.com>
2023-11-04 12:56:58 +0530

    Row component rendered and fetched all url requests

--- src/App.js
+++ src/App.js
@@ -10,8 +10,14 @@ function App() {
 		<div className="app">
 			<Nav />
 			<Banner/>
-			<Row />
-			
+			<Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
+			<Row title="Trending" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
 		</div>
 	);
 }
